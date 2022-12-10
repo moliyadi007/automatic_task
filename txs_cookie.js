501b82dc04cfdde85c43db4d1b7833c6bac28247
moliyadi007 <56004781+moliyadi007@users.noreply.github.com>
2022-12-10 13:05:56 +0800

    Create  txs_cookie.js

---
+++ txs_cookie.js
@@ -0,0 +1,60 @@
+$notification.post('脚本启动','','')
+const cookieName = '淘小说'
+const tokenKey = 'kxsKey'
+const uidKey = 'uidKey'
+
+const moliyadi = init()
+moliyadi.log('运行到这里')
+moliyadi.msg(cookieName, `获取刷新链接: 成功`, '')
+if ($request && $request.method != 'POST' && $request.url.indexOf('itaoxiaoshuo.com/regIds') >= 0) {
+  const body = $request.body.loadJSON()
+  //const tokenheaderVal = JSON.stringify($request.headers)
+  //if (tokenurlVal) moliyadi.setdata(tokenurlVal, tokenurlKey)
+  //if (tokenheaderVal) moliyadi.setdata(tokenheaderVal, tokenheaderKey)
+  moliyadi.msg(cookieName, `获取刷新链接: 成功`, body)
+}
+
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+moliyadi.done()
