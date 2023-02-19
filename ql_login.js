e322c55d55d5e6fcdabe75240caaed1c9780767c
moliyadi007 <56004781+moliyadi007@users.noreply.github.com>
2023-02-19 12:17:58 +0800

    Update ql_login.js

--- ql_login.js
+++ ql_login.js
@@ -1,142 +1,402 @@
 
-$.ql = {
-  type: 'api',
-  headers: {
-    'Content-Type': `application/json;charset=UTF-8`,
-    Authorization: '',
-  },
-  disabled(ids) {
-    if (!this.headers.Authorization) return;
-    const opt = {
-      url: `${$.ql_url}/${this.type}/envs/disable`,
-      headers: this.headers,
-      body: JSON.stringify(ids),
-    };
-    return $.http.put(opt).then((response) => JSON.parse(response.body));
-  },
-  enabled(ids) {
-    if (!this.headers.Authorization) return;
-    const opt = {
-      url: `${$.ql_url}/${this.type}/envs/enable`,
-      headers: this.headers,
-      body: JSON.stringify(ids),
-    };
-    return $.http.put(opt).then((response) => JSON.parse(response.body));
-  },
-  delete(ids) {
-    if (!this.headers.Authorization) return;
-    const opt = {
-      url: `${$.ql_url}/${this.type}/envs`,
-      headers: this.headers,
-      body: JSON.stringify(ids),
-    };
-    return $.http.delete(opt).then((response) => JSON.parse(response.body));
-  },
-  add(records) {
-    if (!this.headers.Authorization) return;
-    const opt = {
-      url: `${$.ql_url}/${this.type}/envs`,
-      headers: this.headers,
-      body: JSON.stringify(records),
-    };
-    return $.http.post(opt).then((response) => JSON.parse(response.body));
-  },
-  edit(records) {
-    if (!this.headers.Authorization) return;
-    const opt = {
-      url: `${$.ql_url}/${this.type}/envs`,
-      headers: this.headers,
-      body: JSON.stringify(records),
-    };
-    return $.http.put(opt).then((response) => JSON.parse(response.body));
-  },
-  select(searchValue) {
-    if (!this.headers.Authorization) return;
-    const opt = {
-      url: `${$.ql_url}/${this.type}/envs?searchValue=${searchValue}`,
-      headers: this.headers,
-    };
-    return $.http.get(opt).then((response) => JSON.parse(response.body));
-  },
-};
+/*
+青龙 docker 每日自动同步 boxjs cookie
+40 * * * https://raw.githubusercontent.com/dompling/Script/master/jd/ql_cookie_sync.js
+ */
+//value_old,value_new
 
-try {
-  $.ql_config = JSON.parse($.read('#ql'));
-} catch (e) {
-  $.ql_config = {};
+console.log('开始执行sync脚本','','')
+const $ = new API('ql', true);
+
+//获取到新的cookie后跟本地的做比较
+//先在本地做更新
+//select云端对应的cookie，跟本地进行比较，删除旧的添加新的
+//发送edit请求
+async function getScriptUrl() {
+  const response = await $.http.get({
+    //url: 'https://gitee.com/moriarty007/automatic_task/raw/master/ql_debug.js',
+    url: 'https://raw.githubusercontent.com/moliyadi007/automatic_task/main/ql_login.js',
+  });
+  return response.body;
 }
 
-$.ql_url = $.ql_config.ip;
-if (!$.ql_url.match(/^(http|https)/)) $.ql_url = `http://${$.ql_url}`;
-
-$.application = {
-  client_id: $.ql_config.client_id,
-  client_secret: $.ql_config.client_secret,
-};
-
-$.ql_account = {
-  username: $.ql_config.username,
-  password: $.ql_config.password,
-};
-
-$.log(`地址：${$.ql_url}`);
-
-function noReady() {
-  $.ql = false;
-  $.log('请配置好相关信息');
+update = async function(value_new,name,remarks,separate){
+  console.log('开始执行update方法')
+  //console.log(value_old)
+  const ql_script = (await getScriptUrl()) || '';
+  eval(ql_script);
+  await $.ql.login();
+  console.log('青龙登录成功')
+  let resp = await $.ql.select(name)
+  //console.log(resp)
+  if (resp.data.length==0){
+    //value_new = value_new+separate
+    $.ql.add([{name:name,value:value_new,remarks:remarks}])
+    return
+  }else{
+  let value_cloud = resp.data[0].value
+  var _id = resp.data[0]._id
+  //console.log(_id)
+//   var space = new RegExp(' ','g')//去除空格
+//   value_cloud = value_cloud.replace(space,'')
+//   if(value_old){
+//   var reg = new RegExp(value_old+separate)//去掉旧的,用空字符替换，然后添加新的，这样做可以避免本地值不存在云端导致不替换成新的
+//   value_cloud = value_cloud.replace(reg,'')
+//   value_cloud = value_cloud + separate + value_new
+  
+//   }else{
+  value_cloud=value_cloud+separate+value_new
+ // }
+  //console.log(value_cloud)
+  console.log(value_cloud)
+  console.log(value_new)
+  if(value_cloud.indexOf(value_new)>=0){
+    console.log('云端已有当前cookie，不再添加')
+    $.notify(name, `云端已有当前cookie，不再添加`, '')
+  }else{
+  let res = $.ql.edit({name:name,value:value_cloud,remarks:remarks,_id:_id})
+  console.log(res)
+  console.log('update执行完成')
+  $.notify(name, `更新cookie成功`, '')
+  }
+ }
 }
 
-if ($.ql_config.is_pwd === 'true') {
-  if ($.ql_account.username && $.ql_account.password) {
-    $.ql.login = async () => {
-      const options = {
-        url: `${$.ql_url}/api/user/login`,
-        body: JSON.stringify($.ql_account),
-        headers: {
-          'Content-Type': `application/json;charset=UTF-8`,
-        },
-      };
-      
-      let response = await $.http.post(options);
-      response = JSON.parse(response.body);
-      if (response.code === 200) {
-        $.ql.type = 'api';
-        $.ql.headers.Authorization = `Bearer ${response.data.token}`;
-        $.log(`登陆成功：${response.data.lastaddr}`);
-        $.log(`ip:${response.data.lastip}`);
-      } else {
-        $.log(response);
-        $.log(`登陆失败：${response.message}`);
-      }
+function getURL(api, key = 'api') {
+  return `${baseURL}/${key}/${api}`;
+}
+
+function disabled(ids) {
+  const opt = {
+    url: getURL(`${urlStr}/disable`),
+    headers,
+    body: JSON.stringify(ids),
+  };
+  return $.http.put(opt).then((response) => JSON.parse(response.body));
+}
+
+function ENV() {
+  const isQX = typeof $task !== 'undefined';
+  const isLoon = typeof $loon !== 'undefined';
+  const isSurge = typeof $httpClient !== 'undefined' && !isLoon;
+  const isJSBox = typeof require == 'function' && typeof $jsbox != 'undefined';
+  const isNode = typeof require == 'function' && !isJSBox;
+  const isRequest = typeof $request !== 'undefined';
+  const isScriptable = typeof importModule !== 'undefined';
+  return { isQX, isLoon, isSurge, isNode, isJSBox, isRequest, isScriptable };
+}
+
+function HTTP(defaultOptions = { baseURL: '' }) {
+  const { isQX, isLoon, isSurge, isScriptable, isNode } = ENV();
+  const methods = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS', 'PATCH'];
+  const URL_REGEX =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+  function send(method, options) {
+    options = typeof options === 'string' ? { url: options } : options;
+    const baseURL = defaultOptions.baseURL;
+    if (baseURL && !URL_REGEX.test(options.url || '')) {
+      options.url = baseURL ? baseURL + options.url : options.url;
+    }
+    options = { ...defaultOptions, ...options };
+    const timeout = options.timeout;
+    const events = {
+      ...{
+        onRequest: () => {},
+        onResponse: (resp) => resp,
+        onTimeout: () => {},
+      },
+      ...options.events,
     };
-    
-  } else {
-    noReady();
+
+    events.onRequest(method, options);
+
+    let worker;
+    if (isQX) {
+      worker = $task.fetch({ method, ...options });
+    } else if (isLoon || isSurge || isNode) {
+      worker = new Promise((resolve, reject) => {
+        const request = isNode ? require('request') : $httpClient;
+        request[method.toLowerCase()](options, (err, response, body) => {
+          if (err) reject(err);
+          else
+            resolve({
+              statusCode: response.status || response.statusCode,
+              headers: response.headers,
+              body,
+            });
+        });
+      });
+    } else if (isScriptable) {
+      const request = new Request(options.url);
+      request.method = method;
+      request.headers = options.headers;
+      request.body = options.body;
+      worker = new Promise((resolve, reject) => {
+        request
+          .loadString()
+          .then((body) => {
+            resolve({
+              statusCode: request.response.statusCode,
+              headers: request.response.headers,
+              body,
+            });
+          })
+          .catch((err) => reject(err));
+      });
+    }
+
+    let timeoutid;
+    const timer = timeout
+      ? new Promise((_, reject) => {
+          timeoutid = setTimeout(() => {
+            events.onTimeout();
+            return reject(
+              `${method} URL: ${options.url} exceeds the timeout ${timeout} ms`,
+            );
+          }, timeout);
+        })
+      : null;
+
+    return (
+      timer
+        ? Promise.race([timer, worker]).then((res) => {
+            clearTimeout(timeoutid);
+            return res;
+          })
+        : worker
+    ).then((resp) => events.onResponse(resp));
   }
-} else {
-  if ($.application.client_id && $.application.client_secret) {
-    $.ql.login = async () => {
-      const options = {
-        url: `${$.ql_url}/open/auth/token?client_id=${$.application.client_id}&client_secret=${$.application.client_secret}`,
-        headers: {
-          'Content-Type': `application/json;charset=UTF-8`,
-        },
+
+  const http = {};
+  methods.forEach(
+    (method) =>
+      (http[method.toLowerCase()] = (options) => send(method, options)),
+  );
+  return http;
+}
+
+function API(name = 'untitled', debug = false) {
+  const { isQX, isLoon, isSurge, isNode, isJSBox, isScriptable } = ENV();
+  return new (class {
+    constructor(name, debug) {
+      this.name = name;
+      this.debug = debug;
+
+      this.http = HTTP();
+      this.env = ENV();
+
+      this.node = (() => {
+        if (isNode) {
+          const fs = require('fs');
+
+          return {
+            fs,
+          };
+        } else {
+          return null;
+        }
+      })();
+      this.initCache();
+
+      const delay = (t, v) =>
+        new Promise(function (resolve) {
+          setTimeout(resolve.bind(null, v), t);
+        });
+
+      Promise.prototype.delay = function (t) {
+        return this.then(function (v) {
+          return delay(t, v);
+        });
       };
-      let response = await $.http.get(options);
-      response = JSON.parse(response.body);
-      $.log(response)
-      if (response.code === 200) {
-        $.ql.type = 'open';
-        $.ql.headers.Authorization = `Bearer ${response.data.token}`;
-        $.log(response)
-        $.log(`登陆成功`);
-      } else {
-        $.log(response);
-        $.log(`登陆失败：${response.message}`);
+    }
+
+    // persistance
+
+    // initialize cache
+    initCache() {
+      if (isQX) this.cache = JSON.parse($prefs.valueForKey(this.name) || '{}');
+      if (isLoon || isSurge)
+        this.cache = JSON.parse($persistentStore.read(this.name) || '{}');
+
+      if (isNode) {
+        // create a json for root cache
+        let fpath = 'root.json';
+        if (!this.node.fs.existsSync(fpath)) {
+          this.node.fs.writeFileSync(
+            fpath,
+            JSON.stringify({}),
+            { flag: 'wx' },
+            (err) => console.log(err),
+          );
+        }
+        this.root = {};
+
+        // create a json file with the given name if not exists
+        fpath = `${this.name}.json`;
+        if (!this.node.fs.existsSync(fpath)) {
+          this.node.fs.writeFileSync(
+            fpath,
+            JSON.stringify({}),
+            { flag: 'wx' },
+            (err) => console.log(err),
+          );
+          this.cache = {};
+        } else {
+          this.cache = JSON.parse(
+            this.node.fs.readFileSync(`${this.name}.json`),
+          );
+        }
       }
-    };
-    
-  } else {
-    noReady();
-  }
+    }
+
+    // store cache
+    persistCache() {
+      const data = JSON.stringify(this.cache);
+      if (isQX) $prefs.setValueForKey(data, this.name);
+      if (isLoon || isSurge) $persistentStore.write(data, this.name);
+      if (isNode) {
+        this.node.fs.writeFileSync(
+          `${this.name}.json`,
+          data,
+          { flag: 'w' },
+          (err) => console.log(err),
+        );
+        this.node.fs.writeFileSync(
+          'root.json',
+          JSON.stringify(this.root),
+          { flag: 'w' },
+          (err) => console.log(err),
+        );
+      }
+    }
+
+    write(data, key) {
+      this.log(`SET ${key}`);
+      if (key.indexOf('#') !== -1) {
+        key = key.substr(1);
+        if (isSurge || isLoon) {
+          return $persistentStore.write(data, key);
+        }
+        if (isQX) {
+          return $prefs.setValueForKey(data, key);
+        }
+        if (isNode) {
+          this.root[key] = data;
+        }
+      } else {
+        this.cache[key] = data;
+      }
+      this.persistCache();
+    }
+
+    read(key) {
+      this.log(`READ ${key}`);
+      if (key.indexOf('#') !== -1) {
+        key = key.substr(1);
+        if (isSurge || isLoon) {
+          return $persistentStore.read(key);
+        }
+        if (isQX) {
+          return $prefs.valueForKey(key);
+        }
+        if (isNode) {
+          return this.root[key];
+        }
+      } else {
+        return this.cache[key];
+      }
+    }
+
+    delete(key) {
+      this.log(`DELETE ${key}`);
+      if (key.indexOf('#') !== -1) {
+        key = key.substr(1);
+        if (isSurge || isLoon) {
+          return $persistentStore.write(null, key);
+        }
+        if (isQX) {
+          return $prefs.removeValueForKey(key);
+        }
+        if (isNode) {
+          delete this.root[key];
+        }
+      } else {
+        delete this.cache[key];
+      }
+      this.persistCache();
+    }
+
+    // notification
+    notify(title, subtitle = '', content = '', options = {}) {
+      const openURL = options['open-url'];
+      const mediaURL = options['media-url'];
+
+      if (isQX) $notify(title, subtitle, content, options);
+      if (isSurge) {
+        $notification.post(
+          title,
+          subtitle,
+          content + `${mediaURL ? '\n多媒体:' + mediaURL : ''}`,
+          {
+            url: openURL,
+          },
+        );
+      }
+      if (isLoon) {
+        let opts = {};
+        if (openURL) opts['openUrl'] = openURL;
+        if (mediaURL) opts['mediaUrl'] = mediaURL;
+        if (JSON.stringify(opts) == '{}') {
+          $notification.post(title, subtitle, content);
+        } else {
+          $notification.post(title, subtitle, content, opts);
+        }
+      }
+      if (isNode || isScriptable) {
+        const content_ =
+          content +
+          (openURL ? `\n点击跳转: ${openURL}` : '') +
+          (mediaURL ? `\n多媒体: ${mediaURL}` : '');
+        if (isJSBox) {
+          const push = require('push');
+          push.schedule({
+            title: title,
+            body: (subtitle ? subtitle + '\n' : '') + content_,
+          });
+        } else {
+          console.log(`${title}\n${subtitle}\n${content_}\n\n`);
+        }
+      }
+    }
+
+    // other helper functions
+    log(msg) {
+      if (this.debug) console.log(msg);
+    }
+
+    info(msg) {
+      console.log(msg);
+    }
+
+    error(msg) {
+      console.log('ERROR: ' + msg);
+    }
+
+    wait(millisec) {
+      return new Promise((resolve) => setTimeout(resolve, millisec));
+    }
+
+    done(value = {}) {
+      if (isQX || isLoon || isSurge) {
+        $done(value);
+      } else if (isNode && !isJSBox) {
+        if (typeof $context !== 'undefined') {
+          $context.headers = value.headers;
+          $context.statusCode = value.statusCode;
+          $context.body = value.body;
+        }
+      }
+    }
+  })(name, debug);
 }
