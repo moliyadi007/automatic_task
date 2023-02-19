cbdb340d0fe2094115af3e09334865b2cab743f6
moliyadi007 <56004781+moliyadi007@users.noreply.github.com>
2023-02-19 12:47:49 +0800

    Update ql_sync.js

--- ql_sync.js
+++ ql_sync.js
@@ -44,13 +44,14 @@ update = async function(value_new,name,remarks,separate){
 //   value_cloud = value_cloud + separate + value_new
   
 //   }else{
-  value_cloud=value_cloud+separate+value_new
+  
  // }
   //console.log(value_cloud)
   if(value_cloud.indexOf(value_new)>=0){
     console.log('云端已有当前cookie，不再添加')
     $.notify(name, `云端已有当前cookie，不再添加`, '')
   }else{
+  value_cloud=value_cloud+separate+value_new
   let res = $.ql.edit({name:name,value:value_cloud,remarks:remarks,_id:_id})
   console.log(res)
   console.log('update执行完成')
