7a41808381616d3108a33ab720875163d8ba893a
moliyadi007 <56004781+moliyadi007@users.noreply.github.com>
2023-02-20 18:55:40 +0800

    Update ql_sync.js

--- ql_sync.js
+++ ql_sync.js
@@ -55,7 +55,7 @@ update = async function(value_new,name,remarks,separate){
   let res = $.ql.edit({name:name,value:value_cloud,remarks:remarks,_id:_id})
   console.log(res)
   console.log('update执行完成')
-  $.notify(name, `更新cookie成功`, '')
+  $.notify(remarks, `更新cookie成功`, '')
   }
  }
 }
